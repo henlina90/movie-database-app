a927e62ebc1b1530c495ea8af05cc141060692d7
Lina S. Hen <60205634+henlina90@users.noreply.github.com>
2022-03-17 15:08:39 -0700

    Update MoviesList.js

--- welovemovie-frontend/src/home/MoviesList.js
+++ welovemovie-frontend/src/home/MoviesList.js
@@ -3,19 +3,24 @@ import { Link } from "react-router-dom";
 import ErrorAlert from "../shared/ErrorAlert";
 import { listMovies } from "../utils/api";
 
-// moviesList
+// MoviesList component
 function MoviesList() {
+  // initializing state variables for movies with an empty array as the default value
   const [movies, setMovies] = useState([]);
+  // initializing state variables for error with default value of null
   const [error, setError] = useState(null);
 
+  // effect hook to get listMovies
   useEffect(() => {
     setError(null);
+    // initializing abortController
     const abortController = new AbortController();
     listMovies(abortController.signal).then(setMovies).catch(setError);
-
     return () => abortController.abort();
+    // cleanup
   }, []);
 
+  // mapping through an array of movies 
   const list = movies.map((movie) => (
     <article key={movie.movie_id} className="col-sm-12 col-md-6 col-lg-3 my-2">
       <img
@@ -33,6 +38,7 @@ function MoviesList() {
     </article>
   ));
 
+// rendering list component
   return (
     <main className="container">
       <ErrorAlert error={error} />
