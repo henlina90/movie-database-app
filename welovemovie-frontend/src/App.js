a5634536b32f2b693655ddb66cce0f51c3a24847
Lina S. Hen <60205634+henlina90@users.noreply.github.com>
2022-03-17 14:46:25 -0700

    Update App.js

--- welovemovie-frontend/src/App.js
+++ welovemovie-frontend/src/App.js
@@ -5,6 +5,9 @@ import FullMovie from "./movie/FullMovie";
 import TheaterList from "./theaters/TheaterList";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+// <Router> to define the scope of routing within the app
+// <Route> components to render components based on the URL path
+// Ex: MoviesList component is rendered if the URL path is "/"
 function App() {
   return (
     <Router>
